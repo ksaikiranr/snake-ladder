297ee231b351c510728e5f4348f1330c60cd07d6
K Sai Kiran <ksaikiranr@gmail.com>
2021-03-28 17:20:49 +0530

    Added Snake and Ladder classes

--- pieces.py
+++ pieces.py
@@ -53,3 +53,52 @@ class PieceBase:
         """
         PieceBase.validate_pos(self.get_start(), pos_end)
         self.__pos_y = pos_end
+
+
+class Snake(PieceBase):
+
+    def __init__(self, pos_start: int, pos_end: int):
+        """
+        Constructor for Snake Class
+        :param pos_start: starting position
+        :param pos_end:  ending position
+        """
+        super().__init__(pos_start, pos_end)
+
+    @staticmethod
+    def validate_pos(pos_start: int, pos_end: int) -> None:
+        """
+        Validates the starting and ending positions.
+        :param pos_start: starting position
+        :param pos_end: ending position
+        :return: None
+        """
+        super().validate_pos(pos_start, pos_end)
+        if pos_end >= pos_start:
+            raise ValueError("For a Snake ending position {} should be lesser than starting position {}"
+                             .format(pos_end, pos_start))
+        return
+
+
+class Ladder(PieceBase):
+
+    def __init__(self, pos_start, pos_end):
+        """
+        Constructor for Ladder Class
+        :param pos_start: starting position
+        :param pos_end:  ending position
+        """
+        super().__init__(pos_start, pos_end)
+
+    @staticmethod
+    def validate_pos(pos_start, pos_end):
+        """
+        Validates the starting and ending positions.
+        :param pos_start: starting position
+        :param pos_end: ending position
+        :return: None
+        """
+        super().validate_pos(pos_start, pos_end)
+        if pos_start >= pos_end:
+            raise ValueError("For a Ladder ending position {} should be greater than starting position {}"
+                             .format(pos_end, pos_start))
